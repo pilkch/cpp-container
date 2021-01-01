8d85819bce404eed479a615d190576187b4f004d
Chris Pilkington <chris.pilkington@gmail.com>
2021-01-01 22:31:59 +1100

    Added link to Lizzie Dixon's blog post.

--- contained.c
+++ contained.c
@@ -2,6 +2,8 @@
 /* This code is licensed under the GPLv3. You can find its text here:
    https://www.gnu.org/licenses/gpl-3.0.en.html */
 
+// Stolen from Lizzie Dixon's blog post on Linux containers in C:
+// https://blog.lizzie.io/linux-containers-in-500-loc.html
 
 #define _GNU_SOURCE
 #include <errno.h>
